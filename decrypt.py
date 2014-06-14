eb5b9e51f1fe70ddf0e8a5b0fbf509802e598a86
Jai Luthra <me@jailuthra.in>
2014-06-14 19:22:20 +0530

    Ported to python3

--- decrypt.py
+++ decrypt.py
@@ -1,21 +1,33 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-# Copyright (C) 2013 Jai Luthra <me@jailuthra.in> 
+# Copyright (C) 2013-2014 Jai Luthra <me@jailuthra.in>
 
-cipher = raw_input('\nEnter the cipher text you want to decrypt: ').decode("hex")
-msg = ''
-key = raw_input('Enter the password earlier used in encryption: ')
-i = 0
-j = 0
-for i in range( len(cipher) ):
-    c = ord(cipher[i])
-    k = ord(key[j])
-    m = c^k
-    msg += chr(m)
-    j += 1
-    if ( j==len(key) ):
-        j=0
-           
-print ('The original message is: '+ msg)
+'''Decrypt an ascii-armored onetimepad cipher'''
 
-  
+import binascii
+
+def main():
+    cipher = input('Cipher: ')
+    key = input('Key: ')
+    msg = decrypt(cipher, key)
+    print('Message:', msg)
+
+def decrypt(cipher, key):
+    '''Return plain text message'''
+    i = 0
+    j = 0
+    msg = ''
+    # get back the string from ascii armored cipher
+    cipher = (binascii.unhexlify(cipher.encode('ascii'))).decode('ascii')
+    for i in range(len(cipher)):
+        c = ord(cipher[i])
+        k = ord(key[j])
+        m = c^k
+        msg += chr(m)
+        j += 1
+        if j == len(key):
+            j = 0
+    return msg
+
+if __name__ == "__main__":
+    main()
--- encrypt.py
+++ encrypt.py
@@ -1,21 +1,35 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-# Copyright (C) 2013 Jai Luthra <me@jailuthra.in> 
+# Copyright (C) 2013-2014 Jai Luthra <me@jailuthra.in>
 
-msg = raw_input('\nEnter the message you want to encrypt: ')
-cipher = ''
-key = raw_input('Enter the password to encrypt: ')
-i = 0
-j = 0
-for i in range ( len(msg) ):
-    m = ord(msg[i])
-    k = ord(key[j])
-    c = m^k
-    cipher += chr(c).encode("hex")
-    j += 1
-    if ( j==len(key) ):
-        j=0
-                    
-print ('The encrypted message is: '+ cipher)
+'''Encrypt a text message into ascii-armored cipher text, using onetimepad'''
 
-  
+import binascii
+
+def main():
+    msg = input('Message: ')
+    key = input('Key: ')
+    if (len(key) < len(msg)):
+        print('\nWARNING: It is insecure to have a key shorter than the message')
+    cipher = encrypt(msg, key)
+    print ('Cipher:', cipher)
+
+def encrypt(msg, key):
+    '''Return cipher text'''
+    i = 0
+    j = 0
+    cipher = ''
+    for i in range(len(msg)):
+        m = ord(msg[i])
+        k = ord(key[j])
+        c = m^k
+        cipher += chr(c)
+        j += 1
+        if j == len(key):
+            j = 0
+    # ascii armor the cipher text
+    cipher = (binascii.hexlify(cipher.encode('ascii'))).decode('ascii')
+    return cipher
+
+if __name__ == "__main__":
+    main()
