81cb722fb8067cc678c337c35cb0d3289edeef8a
Jai Luthra <me@jailuthra.in>
2014-06-20 02:40:20 +0530

    Version v1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='onetimepad',
     
-    version='1.1.0',
+    version='1.1.1',
     description='A hacky implementation of One-time pad',
     long_description=open('README.rst').read(),
 
